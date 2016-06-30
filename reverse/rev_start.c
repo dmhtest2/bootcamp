44b5412b45b85e71d641446a47fdaf6dcdc1fc20
Dave Hirsch <git@davehirsch.com>
2016-06-29 22:38:17 -0700

    add rev; lose executables

--- reverse/rev_start.c
+++ reverse/rev_start.c
@@ -4,18 +4,21 @@ int get1line(char s[], int lim) {
   int c, i;
   for (i=0; i<lim-1 && (c=getchar())!=EOF && c!= '\n'; ++i)
     s[i] = c;
-
+/*
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+*/
   s[i] = '\0';
   return i;
 }
 
 int main() {
   char line[1000];
-  int length, i;
+  int length;
   length = get1line(line, 1000);
-  for (i = length - 1; i >= 0; i--) {
-    printf("%c", line[i]);
-  }
-  printf("\n");
+  printf("%c\n", line[length-1]);
+  printf("%d:%s", length, line);
   return 0;
 }
